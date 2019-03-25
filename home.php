1276f86fc00a631672d8a6bff3e60450661227dc
Luke Parker <lukas.parker@iamsoami.com>
2019-03-25 09:56:51 -0400

    home added

---
+++ home.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<title>Home</title>
+</head>
+
+<body>
+<div class="container">
+	<h1>Welcome! Please click a button to continue:</h1>
+	<a href="evaluation.php"><button type="button" class="btn btn-primary">Take Evaluation</button></a>
+	<a href="report.php"><button type="button" class="btn btn-primary">Generate Report</button></a>
+
+</div>
+</body>
+</html>
