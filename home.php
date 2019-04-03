a0f14aacc15d45b83b7d7d349fc985e3aff29a4f
Todd Allen <tca861@gmail.com>
2019-04-03 09:47:27 -0400

    Update home.php

--- home.php
+++ home.php
@@ -1,5 +1,5 @@
-<html>
-
+<!doctype html>
+<html lang="en">
 <head>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<title>Home</title>
