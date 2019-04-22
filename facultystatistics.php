b78070b30bf2bb17daa19b553aec420152298b9d
Luke Parker <lukas.parker@iamsoami.com>
2019-04-21 22:41:32 -0400

    actually fixed it

--- facultystatistics.php
+++ facultystatistics.php
@@ -107,10 +107,10 @@ if ($questions){
 											echo $count . ' responses. (' . ($count / $totcount)*100 . '%)';
 											echo '<br>';
 
-											echo '<script type="text/javascript">',
-												     `'drawChart();',
-												     '</script>`;
-											}
+											// echo '<script type="text/javascript">',
+											// 	     `'drawChart();',
+											// 	     '</script>`;
+											// }
 
                     echo '<br>';
                     break;
