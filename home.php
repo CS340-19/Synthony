3558f7b8ff8f0faf3da358486bf6fe4e0e0e009e
Todd Allen <tca861@gmail.com>
2019-04-21 23:44:14 -0400

    Testing CSS

--- home.php
+++ home.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<link rel="stylesheet" href="new.css">
 	<title>Home</title>
 </head>
 
 <body>
+<div id="nav"></div>
 <div class="container">
 	<h1>Welcome! Please click a button to continue:</h1>
 	<a href="evaluation.php"><button type="button" class="btn btn-primary">Take Evaluation</button></a>
